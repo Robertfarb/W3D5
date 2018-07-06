3a856912e52cb0afd72df2ae4b3caea9122bd4eb
robertfarb <farb.robert@gmail.com>
2018-07-06 16:45:57 -0700

    complete to Phase 1c finalize

--- active_record_lite/lib/01_sql_object.rb
+++ active_record_lite/lib/01_sql_object.rb
@@ -5,18 +5,32 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+
+    @heredoc ||= DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+      #{table_name}
+    SQL
+
+    @heredoc.first.map(&:to_sym)
   end
 
   def self.finalize!
+    columns.each do |column|
+      define_method(column.to_sym) {instance_variable_get("@#{column}")}
+      define_method("#{column.to_sym}=") {|value| instance_variable_set( @attributes[column.to_sym],value)}
+  
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name ||= to_s.tableize
+    @table_name
   end
 
   def self.all
@@ -32,11 +46,14 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |key,val|
+      raise "unknown attribute #{key}" unless SQLObject.columns.include?(key.to_sym)
+    
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
